2569064aa465c870af3d6fdbb224f171396ff966
지준호 <jjho311@naver.com>
2024-04-11 22:27:25 +0900

    Refactor : @component 주석 해제

--- src/main/java/SpringMVC2/Exception/WebServerCustomizer.java
+++ src/main/java/SpringMVC2/Exception/WebServerCustomizer.java
@@ -11,17 +11,18 @@ import org.springframework.stereotype.Component;
 WAS가 예외를 전달 받음 ==> 오류 페이지 정보 확인 ==> 아래에서 지정된 경로를 오류 페이지로써 요청
 이때, 웹 브라우저(클라이언트)는 서버 내부에서 이러한 일이 발생함을 모름
  */
-//@Component
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         //각 오류 페이지 생성
+        ErrorPage errorPage4xx = new ErrorPage(HttpStatus.BAD_REQUEST, "/error-page/4xx");
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
         ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
 
         //에러 페이지 등록
-        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+        factory.addErrorPages(errorPage4xx, errorPage404, errorPage500, errorPageEx);
     }
 }
