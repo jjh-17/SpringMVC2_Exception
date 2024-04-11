6f4f0dfe799030294749a53968c0a82797bfecbf
지준호 <jjho311@naver.com>
2024-04-11 22:24:49 +0900

    Feat : 4xx 에러 관련 매핑 추가

--- src/main/java/SpringMVC2/Exception/servlet/ErrorPageController.java
+++ src/main/java/SpringMVC2/Exception/servlet/ErrorPageController.java
@@ -29,6 +29,13 @@ public class ErrorPageController {
         log.info("dispatchType={}", request.getDispatcherType());
     }
 
+    @RequestMapping("/error-page/4xx")
+    public String errorPage4xx(HttpServletRequest request, HttpServletResponse response) {
+        log.info("errorPage 4xx");
+        printErrorInfo(request);
+        return "error-page/4xx";
+    }
+
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 404");
@@ -48,18 +55,18 @@ public class ErrorPageController {
     오류 발생 시 JSON 객체 대신 오류 페이지 HTML 반환
         ==> produces를 이용하여 클라이언트 요청 HTTP Header의 Accept 값이 application/json일 경우 해당 메서드 호출
     */
-    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String, Object>> errorPage500Api(
-            HttpServletRequest request, HttpServletResponse response) {
-        log.info("API errorPage 500");
-
-        HashMap<String, Object> result = new HashMap<>();
-        Exception ex = (Exception) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
-        result.put("status", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
-        result.put("message", ex.getMessage());
-
-        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
-    }
+//    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<Map<String, Object>> errorPage500Api(
+//            HttpServletRequest request, HttpServletResponse response) {
+//        log.info("API errorPage 500");
+//
+//        HashMap<String, Object> result = new HashMap<>();
+//        Exception ex = (Exception) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+//        result.put("status", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
+//        result.put("message", ex.getMessage());
+//
+//        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+//
+//        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
+//    }
 }
--- src/main/java/SpringMVC2/Exception/servlet/ServletExceptionController.java
+++ src/main/java/SpringMVC2/Exception/servlet/ServletExceptionController.java
@@ -11,35 +11,35 @@ import java.io.IOException;
 @Controller
 public class ServletExceptionController {
 
-    @GetMapping("/servlet")
+    @GetMapping()
     public String servlet_welcome(){
-        log.info("[예외 처리]");
+        log.info("[예외 처리 시작]");
         return "servlet_index";
     }
 
-    @GetMapping("/servlet/error")
+    @GetMapping("/error")
     public void servlet_error() {
-        log.info("예외 발생");
+        log.error("예외 발생");
         throw new RuntimeException("서블릿 예외 발생");
     }
 
-    @GetMapping("/servlet/error-404")
+    @GetMapping("/error-404")
     public void servlet_error404(HttpServletResponse response) throws IOException {
-        log.info("예외 404 발생");
+        log.error("예외 404 발생");
 
         //서블릿 컨테이너에 오류가 발생하였음을 전달
         response.sendError(404, "404 오류");
     }
 
-    @GetMapping("/servlet/error-400")
+    @GetMapping("/error-4xx")
     public void servlet_error400(HttpServletResponse response) throws IOException {
-        log.info("예외 400 발생");
+        log.error("예외 400 발생");
         response.sendError(400, "400 오류");
     }
 
-    @GetMapping("/servlet/error-500")
+    @GetMapping("/error-500")
     public void servlet_error500(HttpServletResponse response) throws IOException {
-        log.info("서블릿 예외 500 발생");
+        log.error("서블릿 예외 500 발생");
         response.sendError(500, "500 오류");
     }
 }
